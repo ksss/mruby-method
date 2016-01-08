bce734d3b9124e5dec9159f45725381117a79622
ksss <co000ri@gmail.com>
2016-01-08 10:23:59 +0900

    Split file

--- mrblib/unbound_method.rb
+++ mrblib/unbound_method.rb
@@ -1,29 +1,10 @@
-class Method
-  def call(*args)
-    @recv.__send__(@name, *args)
-  end
-  alias_method :[], :call
-
-  def to_proc
-    lambda { |*args|
-      self.call(*args)
-    }
-  end
-
+class UnboundMethod
   def owner
     @owner
   end
 
-  def receiver
-    @recv
-  end
-
   def name
     @name
-  end
-
-  def arity
-    @proc.arity
   end
 
   def source_location
